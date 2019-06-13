8b908bbb8fdd8336df8aa0c543703eac742d4184
j6079633 <j6079633@gmail.com>
2019-06-13 14:38:06 +0800

    add Romain2Int

---
+++ RomanToInt.cpp
@@ -0,0 +1,20 @@
+/* Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M
+ * Input roman numeral string and return the value*/
+#include <map>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+class Solution {
+private:
+    map<char, int> relationship = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+public:
+    int romanToInt(string s) {
+        
+    }
+};
+int main(){
+	Solution sol;
+	
+	return 0;
+}
